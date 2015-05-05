94b4447818303c8d714e1775e58f4351459cf3b1
liuwenrong <liuwenrong@tianpingpai.com>
2015-05-05 20:14:04 +0800

    hahah

--- src/com/example/test2/MainActivity.java
+++ src/com/example/test2/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // hhahahhahahhahahahh
         setContentView(R.layout.activity_main);
     }
 
